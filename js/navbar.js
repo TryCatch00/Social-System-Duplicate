132fb23c8f0863ffae2b8ddd4f621d34913bae3d
Yavuz Selim <pounter@post.com>
2024-09-17 22:53:09 +0300

    İmportant Changes :)

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,11 @@
-const focusSpanSearch = document.getElementById("focus-search");
-const searchInput = document.getElementById("searchInput");
 const navbar = document.getElementById("navbar");
 
-let isLogin = false;
+let isLogin = true;
 if (!isLogin) {
     let innerHTML = `
         <div id="navbar-title">
-            <button id="left-bar-button" class="button-class five-button"><img src="img/icon/menu-icon.svg" alt="Bar"></button>
-            <a href="index.html" class="button-class info-element-button" id="home-page-rooter"><span class="info-element" style="bottom: -15px;">Home Page</span>Social System <span id="country-lower">EN</span></a>
+            <button id="left-bar-button" class="button-class five-button info-element-button"><img src="img/icon/menu-icon.svg" alt="Bar"><span class="info-element">Menu</span></button>
+            <a href="index.html" class="button-class info-element-button" id="home-page-rooter"><span class="info-element">Home Page</span>Social System <span id="country-lower">EN</span></a>
         </div>
         <div id="search-bar">
             <div id="by-search">
@@ -16,9 +14,9 @@ if (!isLogin) {
                     <input type="text" placeholder="Search" id="searchInput" oninput="toggleResetButton(this.value)" autocomplete="off">
                     <button class="button-class five-button" id="resetButton" onclick="resetInput()" disabled><img src="img/icon/close-icon.svg" alt=""></button>
                 </div>
-                <button class="button-class info-element-button button-class-bg" id="search-button"><span class="info-element" style="bottom: -20px;">Search</span><img src="img/icon/search-icon.svg" alt="Search"></button>
+                <button class="button-class info-element-button button-class-bg" id="search-button"><span class="info-element">Search</span><img src="img/icon/search-icon.svg" alt="Search"></button>
             </div>
-            <button id="mic-button" class="button-class five-button info-element-button button-class-bg"><span class="info-element" style="bottom: -20px;">Make a Voice Call</span><img src="img/icon/mic-icon.svg" alt="Mic"></button>
+            <button id="mic-button" class="button-class five-button info-element-button button-class-bg"><span class="info-element">Make a Voice Call</span><img src="img/icon/mic-icon.svg" alt="Mic"></button>
         </div>
         <div id="tools">
             <button class="five-button button-class info-element-button" id="create-video"><span class="info-element">More</span><img src="img/icon/more-icon.svg" alt="Add"></button>
@@ -27,30 +25,10 @@ if (!isLogin) {
     `;
 
     navbar.innerHTML = innerHTML;
+    listenerFocus();
 }
 
-let mobileInnerHTML = `
-    <div id="navbar-title">
-        <a href="index.html" class="button-class" id="home-page-rooter" style="padding: 10px 10px;">Social System <span id="country-lower">EN</span></a>
-    </div>
-    <div id="search-bar">
-        <button id="mobile-mic-button" class="button-class five-button"><img src="img/icon/mic-icon.svg" alt="Mic"></button>
-        <button class="five-button button-class" id="notification"><img src="img/icon/notification-icon.svg" alt="Anc"><span id="intensity-icon">+0</span></button>
-        <button class="button-class five-button" id="mobile-search-button"><img src="img/icon/search-icon.svg" alt="Search"></button>
-    </div>
-`;
-
 const desktopInnerHTML = navbar.innerHTML;
-
-searchInput.addEventListener("focus", () => {
-    focusSpanSearch.innerHTML = `<img src="img/icon/search-icon-blue.svg" style="padding: 2px;" alt="Search Icon">`;
-    searchInput.placeholder = "Searching...";
-});
-
-searchInput.addEventListener("blur", () => {
-    focusSpanSearch.innerHTML = ``;
-    searchInput.placeholder = "Search";
-});
 
 function toggleResetButton(value) {
     const resetButton = document.getElementById('resetButton');
@@ -66,6 +44,7 @@ function toggleResetButton(value) {
 function resetInput() {
     console.log("Developer Console: Active system please close.");
 
+    const searchInput = document.getElementById("searchInput");
     if (searchInput.value !== "") {
         searchInput.value = "";
         toggleResetButton("");
@@ -76,12 +55,38 @@ const mediaQuery = window.matchMedia('(max-width: 991px)');
 
 function handleMediaChange(e) {
     if (e.matches) {
+        let mobileInnerHTML = `
+            <div id="navbar-title">
+                <a href="index.html" class="button-class" id="home-page-rooter" style="padding: 10px 10px;">Social System <span id="country-lower">EN</span></a>
+            </div>
+            <div id="search-bar">
+                <button id="mobile-mic-button" class="button-class five-button"><img src="img/icon/mic-icon.svg" alt="Mic"></button>
+                <button class="five-button button-class" id="notification"><img src="img/icon/notification-icon.svg" alt="Anc"><span id="intensity-icon">+0</span></button>
+                <button class="button-class five-button" id="mobile-search-button"><img src="img/icon/search-icon.svg" alt="Search"></button>
+            </div>
+        `;
         navbar.innerHTML = mobileInnerHTML;
     } else {
         navbar.innerHTML = desktopInnerHTML;
+        listenerFocus();
     }
 }
 
 handleMediaChange(mediaQuery);
-
 mediaQuery.addListener(handleMediaChange);
+
+
+function listenerFocus() {
+    const searchInput = document.getElementById("searchInput");
+    const focusSpanSearch = document.getElementById("focus-search");
+
+    searchInput.addEventListener("focus", () => {
+        focusSpanSearch.innerHTML = `<img src="img/icon/search-icon-blue.svg" style="padding: 2px;" alt="Search Icon">`;
+        searchInput.placeholder = "...";
+    });
+
+    searchInput.addEventListener("blur", () => {
+        focusSpanSearch.innerHTML = ``;
+        searchInput.placeholder = "Search";
+    });
+}
